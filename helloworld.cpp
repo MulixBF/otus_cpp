58d5c7ce40908bf0992060aae50d0c61cdad6679
Andrey Mulenkov <amulenkov@wanna.by>
2021-01-05 19:31:00 +0300

    added cmake project

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+//
+//  helloworld.cpp
+//  otus_cpp
+//
+//  Created by Andrey Mulenkov on 05.01.2021.
+//
+
+#include <stdio.h>
+
+
+int main() {
+
+    std::cout << "build" << BUILD_NUMBER << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+}
