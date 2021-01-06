d89785992d3b053d3fc211c77b79a8176ce59752
Andrey Mulenkov <amulenkov@wanna.by>
2021-01-06 13:16:32 +0300

    fixed build

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,15 +1,8 @@
-//
-//  helloworld.cpp
-//  otus_cpp
-//
-//  Created by Andrey Mulenkov on 05.01.2021.
-//
-
-#include <stdio.h>
+#include <iostream>
 
 
 int main() {
 
-    std::cout << "build" << BUILD_NUMBER << std::endl;
+    std::cout << "build " << BUILD_NUMBER << std::endl;
     std::cout << "Hello, World!" << std::endl;
 }
